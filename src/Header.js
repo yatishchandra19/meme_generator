37caef320ceb8a548659b5f30963663bb4a7962a
yatishchandra19 <yatishchandra19@gmail.com>
2024-04-09 16:12:36 +0530

    main commit

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="head-section">
       <img src="./images/troll-face.svg" className="logo"></img>
       <h1 className="proj-name">Meme Generator</h1>
-      <p className="proj-desc">React Course - Project 3</p>
+      <p className="proj-desc">React - Project</p>
     </header>
   );
 }
